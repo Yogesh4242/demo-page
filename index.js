59ffa00148073cf1f9157d36ac9a398b71c96c96
Yogesh4242 <90864309+Yogesh4242@users.noreply.github.com>
2024-12-11 00:17:55 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,63 @@
+// //console.log("hello world")
+
+
+// // Data types 
+
+// let isapproved = true;
+
+// let firstname = undefined ;
+
+// let selcol = null;
+
+
+// // Object 
+
+// let person = {
+
+//     name:'yogesh',
+//     age:20
+
+// }
+
+// person.age=19 
+
+
+// console.log(person)
+
+
+// // array 
+
+// let selectedcolour = ['red', 'blue']
+
+// selectedcolour [2] = 'green';
+
+// console.log (selectedcolour)
+
+
+// //calculating a value square 
+
+// function square(number){
+
+//     return number * number ;
+// }
+
+// let number = square(5)
+
+// console.log(number);
+
+// // OR 
+
+// console.log(square(2));
+
+
+
+function opennewtab() {
+
+    window.open("tocheck.html","_blank");
+ }
+
+
+
+
+
+
